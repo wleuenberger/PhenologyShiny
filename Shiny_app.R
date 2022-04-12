32dd70c8057df6e8624b2cb1c99e992431f8897e
wleuenberger <leuenbe9@msu.edu>
2022-04-12 09:42:33 -0400

    Added SimplePlot dataframe for testing

--- Shiny_app.R
+++ Shiny_app.R
@@ -1,32 +1,55 @@
 # Load shiny
 library(shiny)
+library(magrittr)
+library(tidyverse)
 
 # Get data
-phen<-read.csv("/Users/karachristinad/Library/CloudStorage/OneDrive-MichiganStateUniversity/CSS 844/Module 3/PhenologyData/CleanedPhenologyData2017to2021.csv")
+# Paths to our individual computers
+WendyPath <- 'C:/Users/Wendy/OneDrive\ -\ Michigan\ State\ University/GitHub/PhenologyShiny/'
+KaraPath <- "/Users/karachristinad/Library/CloudStorage/OneDrive-MichiganStateUniversity/CSS 844/Module 3/PhenologyData/"
+
+# Change Path to your path for the code 
+phen<-read.csv(paste0(WendyPath, "CleanedPhenologyData2017to2021.csv"))
+
+# Make simpler data to play with
+SimplePlot <- phen %>% 
+  group_by(SPECIES, species, Year, Week) %>% 
+  summarize(ColorR5 = mean(ColorR5),
+            FallR5 = mean(FallR5)) %>% 
+  pivot_longer(cols = c(ColorR5, FallR5), 
+               names_to = 'Measurement',
+               values_to = 'Values')
 
 # Define UI
 ui <- pageWithSidebar(
         headerPanel('Phenology'),
         sidebarPanel(width = 4,
-             selectInput('SPECIES', 'Choose a species:',paste(phen$SPECIES)),
+             selectInput('SPECIES', 'Choose a species:',paste(unique(phen$SPECIES))),
              selectInput("variable", "Variable:", 
-                         c("Leaf Color" = "color",
-                           "Leaf Fall" = "fall"))),
+                         c("Leaf Color" = "ColorR5",
+                           "Leaf Fall" = "FallR5"))),
              mainPanel())
 
 # Define server
 server <- function(input, output) {
-        formulaText <- reactive({
-                paste("temp ~", input$variable)
+        selectedData <- reactive({
+          SimplePlot %>% 
+            filter(SPECIES == input$SPECIES,
+                   Measurement == input$variable)
         })
+    
+        # formulaText <- reactive({
+        #         paste("Values ~ Week")
+        # })
+        formulaText <- 'Values ~ Week'
         
         output$caption <- renderText({
-                formulaText()
+                formulaText
         })
         
         output$plot <- renderPlot({
-                plot(as.formula(formulaText()),
-                     data=phen)
+                plot(as.formula(formulaText),
+                     data=selectedData)
         })
         
 }
